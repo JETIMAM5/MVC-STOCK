57a2011e6e1c0d5992cb442a762b0f5f35768c50
JETIMAM <127851941+JETIMAM5@users.noreply.github.com>
2023-09-05 10:23:55 +0300

    Added Popup modals & SalesContoller

---
+++ MVC-STOCK/Controllers/SalesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_STOCK.Models.Entity;
+
+namespace MVC_STOCK.Controllers
+{
+    public class SalesController : Controller
+    {
+        MvcDbStockEntities db = new MvcDbStockEntities();
+        // GET: Sales
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult NewSale()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewSale(TBLSALES p)
+        {
+            db.TBLSALES.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
